e33b6930eb7a8c67c5dc7c4d4160ec4161e1d2d3
patricia figueroa <cpfigueroa1@gmail.com>
2024-05-14 17:35:03 -0300

    landing page diseño

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-// app.jsx
 import React, { useState } from 'react';
-import './App.css'; // Asegúrate de importar el archivo CSS
-import Home from  './Components/Home/Home.jsx'; // Ajusta la ruta según tu estructura de archivos
+import './App.css'; 
+import LandingPage from './Components-0/LandingPage/LandingPage';
+import ScrollReveal from 'scrollreveal';
+
 
 function App() {
+  useEffect(() => {
+    ScrollReveal().reveal('.my-element', { delay: 200 });
+  }, []);
 
   return (
-    <div className='APP'>
-      <Home/>
+    <div className='my-element'>
+      <LandingPage/>
     </div>
 
   );
--- src/Components/Nav/Nav.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Nav = () => {
-  return (
-    <div>EjemploNav</div>
-  )
-}
-
-export default Nav
