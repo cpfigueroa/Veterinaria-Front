a102ff1d35f5745a7db069a15d6567e7fb532db5
Matheo Such <matheosuch1@gmail.com>
2024-05-16 19:15:53 -0300

    Primer codigo en AdminHome.jsx y prueba de exportar e importar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import './App.css'; 
+import './App.css';
 import ScrollReveal from 'scrollreveal';
-
+import AdminHome from './admin/AdminHome';
 
 function App() {
- 
-
-  return (
-    <div className='my-element'>
-      soy app
-    </div>
-
-  );
+	return (
+		<div className="my-element">
+			soy app
+			<AdminHome />
+		</div>
+	);
 }
 
 export default App;
