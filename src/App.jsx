fe0118616358aae4c81e0e073bca88120861f921
Matheo Such <matheosuch1@gmail.com>
2024-05-16 20:10:00 -0300

    Estructura básica y prueba de importación de PatientCRUD.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import ScrollReveal from 'scrollreveal';
 import AdminHome from './admin/AdminHome';
+import PatientCRUD from './admin/PatientCRUD';
 
 function App() {
 	return (
 		<div className="my-element">
 			soy app
 			<AdminHome />
+			<PatientCRUD />
 		</div>
 	);
 }
